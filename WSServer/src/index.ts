407c20422ceb96814f2e7c27a8d42573dedfc4e5
superfroggman <49985901+superfroggman@users.noreply.github.com>
2020-09-14 13:50:21 +0200

    added cool latencty and FPS test

--- WSServer/src/index.ts
+++ WSServer/src/index.ts
@@ -2,9 +2,27 @@ import WebSocket from 'ws'
 
 const wss = new WebSocket.Server({ port: 8080 })
 
+const chunkSize: number = 1000
+
+
+console.log("Server started")
+
+
+
 wss.on('connection', ws => {
+    var cooldate: number = Date.now();
+
   ws.on('message', message => {
-    console.log(`Received message => ${message}`)
+    if(parseInt(message.toString()) % chunkSize == 0 && parseInt(message.toString())!=0){
+        console.log(`Received message => ${message}`)
+        var time = Date.now() - cooldate
+        cooldate = Date.now()
+
+        var coolertime = time/chunkSize
+
+        console.log(`ms/request: ${coolertime}\nFPS: ${1000/coolertime}\n`)
+    }
+    
     ws.send('Je suis une Typescript server!') 
   })
  
