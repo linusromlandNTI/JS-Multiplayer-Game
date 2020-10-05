8df0d03a206fb7df8751a2db7fccf68b989e569d
superfroggman <anrehelenius@gmail.com>
2020-10-05 18:56:33 +0200

    added text above players in canvas

--- Client/src/render.ts
+++ Client/src/render.ts
@@ -1,9 +1,15 @@
+let pWidth = 20;
+let pHeight = 20;
+
 function render(message: string) {
   var c = <HTMLCanvasElement>document.getElementById("mainCanvas");
   var ctx = c.getContext("2d");
 
   if (ctx) {
     ctx.clearRect(0, 0, c.width, c.height);
+
+    ctx.font = "14px Arial";
+    ctx.textAlign = "center";
 
     let jsonMessage = JSON.parse(message);
 
@@ -12,7 +18,8 @@ function render(message: string) {
     for (let i = 0; i < jsonMessage.players.length; i++) {
       let player = jsonMessage.players[i];
       //console.log("name: " + player.name);
-      drawRect(ctx, player.x, player.y, 20, 20);
+      drawRect(ctx, player.x, player.y, pWidth, pHeight);
+      ctx.fillText(player.name, player.x, player.y-(pHeight/2)); 
     }
   }
 }
