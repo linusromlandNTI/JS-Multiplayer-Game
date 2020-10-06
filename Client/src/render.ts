d389420a4b4a27a6b57c1303ffd5abd6be98833a
superfroggman <anrehelenius@gmail.com>
2020-10-06 14:07:05 +0200

    added drawing stamina bar

--- Client/src/render.ts
+++ Client/src/render.ts
@@ -28,17 +28,50 @@ function render(message: string) {
     //Loop through list of players and draw everyone
     for (let i = 0; i < jsonMessage.players.length; i++) {
       let player = jsonMessage.players[i];
-      if(player.stamina < 0) player.stamina = 0
-      let name = player.name + "\n" + player.stamina.toFixed(0);
+      if (player.stamina < 0) player.stamina = 0;
+      let name = player.name;
       //Limit name to 15 characters
       if (name.length > 15) name = name.substr(0, 15);
       //Draw name
-      ctx.fillText(name, player.x + pWidth / 2, player.y - 2);
-
+      ctx.fillText(name, player.x + pWidth / 2, player.y - 20);
+      //Draw stamina bar
+      drawRect(ctx, player.x, player.y - 6, pWidth, 4, false, "black");
+      drawRect(
+        ctx,
+        player.x,
+        player.y - 6,
+        pWidth * (player.stamina / 100),
+        4,
+        true,
+        "green"
+      );
       //Draw player as image
       const image = new Image();
       image.src = "res/guy.png";
       ctx.drawImage(image, player.x, player.y, pWidth, pHeight);
     }
   }
+}
+
+//Draw rectangle
+function drawRect(
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  w: number,
+  h: number,
+  filled: boolean,
+  color: string
+) {
+  let defaultColor = "black";
+
+  ctx.beginPath();
+  ctx.rect(x, y, w, h);
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+  if (filled) ctx.fill();
+  else ctx.stroke();
+
+  ctx.strokeStyle = defaultColor;
+  ctx.fillStyle = defaultColor;
 }
